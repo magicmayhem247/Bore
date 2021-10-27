f9e358e87f7f7716b2ba5995675668c2a4561662
magicmayhem247 <69684684+magicmayhem247@users.noreply.github.com>
2021-10-27 15:47:03 +0100

    Initial commit

---
+++ src/main/java/com/magicmayhem247/bore/Bore.java
@@ -0,0 +1,36 @@
+package com.magicmayhem247.bore;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod(Bore.MOD_ID)
+public class Bore
+{
+    public static final String MOD_ID = "bore";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Bore() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
